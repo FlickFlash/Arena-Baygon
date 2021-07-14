0768230a8a164436027ce06f6616b8a4e5de6799
FlickFlash <85416533+FlickFlash@users.noreply.github.com>
2021-07-14 00:28:32 -0300

    Add files via upload
    BIG UPDATE: 99% of web design responsiveness now, every main page was adjusted to fit smartphone screens (based on an iPhone 8 virtual extension);
    Side menu on "Videos" is not a side menu anymore when the screen gets stretched;
    Table on "Rankings" adjusted to fit screen, but it gets too small and tight on a smartphone screen;
    Page "Partidas" now has two versions: one for smartphones, and another for devices with wider screens; the first version displays team players in line without soccer field background, the second has team players perfectly aligned where they should be displayed on the soccer field background;
    Some placeholders on "Registro" were rewritten so they wouldn't exceed the available space.

---
+++ xExtra/partidas.js
@@ -0,0 +1,12 @@
+function testeTexto() {
+    let textoJogo = window.document.getElementById('textoCampoID')
+    let textoInfo = document.getElementById('textoInfo')
+    textoInfo.innerHTML = ''
+    textoJogo.innerHTML += `ahusahsuasu `
+}
+
+// ETAPA 1 - DEFINIÇÃO DE CORES DOS TIMES
+// ETAPA 2 - DEFINIÇÃO DE JOGADORES DO TIME
+// REPETIÇÃO DAS ETAPAS 1 E 2 ATÉ TODOS OS TIMES E JOGADORES TEREM SIDO REGISTRADOS
+// ETAPA 3 - REGISTRO DOS EVENTOS RELEVANTES DO JOGO
+// ETAPA 4 - O SISTEMA SOMA OS GOLS POR EQUIPE E DEFINE VITÓRIA DE UM DOS TIMES OU EMPATE
