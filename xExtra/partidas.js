b24e8126ae5a915416a2b8a303fe85b86fd0e148
FlickFlash <85416533+FlickFlash@users.noreply.github.com>
2021-07-21 22:46:52 -0300

    Add files via upload
    Variables are now used whenever possible on "partidas.js", options(select) are simplified by the use of variables.

--- xExtra/partidas.js
+++ xExtra/partidas.js
@@ -1,82 +1,191 @@
+let matchNumber = 0
+let teamNumber = 0
+var todasCoresTimes = ['','Time Azul', 'Time Roxo', 'Time Vermelho']
+var todasCoresCirculos = ['', '&#128309;', '&#128995;', '&#128308;']
+var todasCoresBackground = ['', 'steelblue', 'slateblue', 'red']
+var jogadoresBaygon = ['', 'Adilson', 'Zé', 'Ályson', 'Tuta', 'Fernando', 'Dau', 'Milson', 'Ari', 'Iago', 'Antonio', 'Jaedson', 'Igor'].sort(function (a, b) {
+    return a.localeCompare(b);
+});
+const jogadorPosition = ['', 'Goleiro', 'Defensor 1', 'Defensor 2', 'Atacante 1', 'Atacante 2']
+let contagemTimes = 0
+let contagemJogadores = 0
+
+var totalTimes
+var jogadoresEmCadaTime
+
+console.log(jogadoresBaygon)
+
+var todosTimes = {
+    Time0:[''], 
+    Time1:['Zé'], 
+    Time2:['']
+};
+
+todosTimes.Time1.push('Adilson')
+console.log(todosTimes.Time1)
+
+$(function (e) {
+    $("")
+});
 
 function initConfig() {
-    //Removing Initial Structure
-    let button1 = document.getElementById("configStartArt");
-    button1.remove(0)
+    //Removing Initial Button
+    $(function (e) {
+        $("button#initConfigButton").remove();
+        $("div#initConfigDiv").remove();
+    });
+
+    //Settings' Title
+    $(function (e) {
+        $("article#configStartArt").append("<header style='text-align:center;'><h2>Configurações de Times</h2></header>")
+    });
+
+    //Total Number of Teams
+    $(function (e) {
+        $("article#configStartArt").append("<div id='configNumeroTimes' style='margin: 5px'>Número total de times: <input id='inputTotalTimes' type='number' min='2' max='8'><button class='confirm-setup' onclick='jogadoresPorTime()'>OK</button></div>")
+    });
+
+
+};
+
+function jogadoresPorTime() {
+    totalTimes = Number(document.getElementById("inputTotalTimes").value)
+    if (totalTimes < 2 || totalTimes > 8) {
+        alert('Escolha um número de times de 2 a 8');
+    } else {
+        $(function (e) {
+            $("div#configNumeroTimes").remove();
+            $("article#configStartArt").append(`<div style='margin: 5px;'>Número total de times: ${totalTimes}</div>`)
+            $("article#configStartArt").append("<div id='configJogadoresPorTime' style='margin: 5px'>Número de jogadores em cada time: <input id='inputJogadoresPorTime' type='number' min='5' max='11'><button class='confirm-setup' onclick='colorChoice()'>OK</button></div>")
+        });
+    };
+};
+
+function colorChoice() {
+    jogadoresEmCadaTime = Number(document.getElementById("inputJogadoresPorTime").value)
+    //First Choice of Team Color
+    if (jogadoresEmCadaTime < 5 || jogadoresEmCadaTime > 11) {
+        alert('Escolha de 5 a 11 jogadores por time')
+    } else {
+        teamNumber++
+        $(function (e) {
+            $("div#configJogadoresPorTime").remove();
+            $("article#configStartArt").append(`<div style='margin: 5px;'>Número de jogadores por time: ${jogadoresEmCadaTime}</div>`)
+            $("article#configStartArt").append("<form id='formTeam1' style='display: inline-block;'></form>");
+            $("form#formTeam1").append("<label for='corTimeID1'>Monte o 1º time: <label>");
+            $("form#formTeam1").append("<select id='corTimeID1' name='escolhaCorTimeID1'></select>");
+            $("select#corTimeID1").append("<option value='' selected='selected' disabled='disabled'>Selecione a cor</option>");
+            let iColorOptions = 1
+            while (iColorOptions < todasCoresTimes.length) {
+                $("select#corTimeID1").append(`<option value='${iColorOptions}' style='background-color: ${todasCoresBackground[iColorOptions]};'>${todasCoresCirculos[iColorOptions]} ${todasCoresTimes[iColorOptions]}</option>`);
+                    iColorOptions++
+            /*$("select#corTimeID1").append("<option value='' style='background-color:'></option>")
+            $("select#corTimeID1").append(`<option value='2' style='background-color: ${todasCoresBackground[2]};'>${todasCoresCirculos[2]} ${todasCoresTimes[2]}</option>`);
+            $("select#corTimeID1").append(`<option value='3' style='background-color: ${todasCoresBackground[3]};'>${todasCoresCirculos[3]} ${todasCoresTimes[3]}</option>`);*/
+            }
+        });
+
+        // Confirm Button
+        $(function (e) {
+            $("article#configStartArt").append("<button id='confirmTeamColorID1' class='confirm-setup' onclick='selecionarJogadores()'>OK</button>");
+        });    
+    }
+};
+
+
+function selecionarJogadores() {
+    //Removal of Form for Color of Team 1
+    let escolhaCor1 = document.getElementById("corTimeID1").value
+    $(function (e) {
+        $("form#formTeam1").remove();
+        $("button#confirmTeamColorID1").remove();
+        $("article#configStartArt").append(`<div id='definindoTime1' class="choose-players-title"><strong>${todasCoresCirculos[escolhaCor1]}${todasCoresTimes[escolhaCor1]}</strong>:</div>`);
+    });
     
+    //Adding Team Players
+    $(function (e) {
+        contagemJogadores++
+        $("article#configStartArt").append("<form id='formPlayer1T1'></form>");
+        $("form#formPlayer1T1").append(`<label for='player1TimeID1'>${jogadorPosition[contagemJogadores]} do 1º time: <label>`);
+        $("form#formPlayer1T1").append("<select id='player1TimeID1' name='escolhaJogador1TimeID1'></select>");
+        $("select#player1TimeID1").append(`<option value='' selected='selected' disabled='disabled'>Selecione o ${jogadorPosition[contagemJogadores]}</option>`);
+        let iOptionJogadores = 1
+        while (iOptionJogadores < jogadoresBaygon.length) {
+            $("select#player1TimeID1").append(`<option value='${iOptionJogadores}'>${todasCoresCirculos[escolhaCor1]} ${jogadoresBaygon[iOptionJogadores]}</option>`);
+            iOptionJogadores++
+        }
+            $("select#player1TimeID1").append(`<option value='${iOptionJogadores}'>Indefinido</option>`);
+            /*$("select#player1TimeID1").append("<option value='' style='background-color:'></option>")*/
+            /* $("select#player1TimeID1").append(`<option value='2' style='background-color: ${todasCoresBackground[escolhaCor1]};'>${todasCoresCirculos[escolhaCor1]} ${jogadoresBaygon[iOptionJogadores]}</option>`);*/
+            /* &#128308; Acrescentar círculo colorido dinamicamente de acordo com cor de time escolhida */
+    });
+
+    // Confirm Button
+    $(function (e) {
+        $("article#configStartArt").append("<button id='confirmPlayer1T1ID' class='confirm-setup' onclick='jogadorSelecionado()'>OK</button>");
+    });    
+
+};
+function jogadorSelecionado() {
+    let escolhaJogador1 = document.getElementById("player1TimeID1").value;
+    var defTime1 = document.getElementById("definindoTime1");
+    if  (escolhaJogador1 == "") {
+        alert('Escolha uma opção válida!');
+    } else if (Number(escolhaJogador1) == Number(jogadoresBaygon.length)){
+        defTime1.innerHTML += ` Indefinido`
+    } else {
+        $(function (e) {
+            
+            
+            defTime1.innerHTML += ` ${jogadoresBaygon[escolhaJogador1]}`;
+
+        });    
+    };
+};
+
+
+function initMatches() {
+    matchNumber++
+
     //Adding New Section to New Match
-    let dinamicSection = document.createElement("section");
-    dinamicSection.setAttribute("class", "quadro-jogo");
-    dinamicSection.setAttribute("id", "infoTable"); // I need to rename newly created sections
-    document.getElementById("allMatches").appendChild(dinamicSection);
+    $(function (e) {
+        $("main#allMatches").prepend("<section id='infoTable"+matchNumber+"' class='quadro-jogo'></section>"); //Still need dynamic ID 
+    });
 
     // Adding Title of the Match
-    let dinamicHeader = document.createElement("header");
-    dinamicHeader.innerHTML = `<h1><strong>Jogo 1</strong></h1>`;
-    dinamicHeader.setAttribute("id","jogoNumero");
-    /*dinamicHeader.className = "xx-xx";*/
-    document.getElementById("infoTable").appendChild(dinamicHeader);
+    $(function (e) {
+        $("section#infoTable"+matchNumber+"").append(`<header class='jogo-numero'><h1><strong>Jogo ${matchNumber}</strong></h1><header>`);
+    });
 
     //Adding Soccer Field (Background)
-    let showField = document.createElement("aside");
-    document.getElementById("infoTable").appendChild(showField);
+    $(function (e) {
+        $("section#infoTable"+matchNumber+"").append("<aside></aside>");
+    });
 
     // Score of teams come in this position
 
     //Adding Article for Match Events
-    let artMatch = document.createElement("article");
-    artMatch.setAttribute("id", "allEvents");
-    artMatch.setAttribute("class", "all-events")
-    document.getElementById("infoTable").appendChild(artMatch);
-
-    //First Choice of Team Color
-    let labelCor = document.createElement("label");
-    labelCor.setAttribute("for", "corTimeID1");
-    labelCor.innerHTML = "Selecione a cor do time 1: ";
-
-    let corTime1 = document.createElement("select");
-    corTime1.setAttribute("id","corTimeID1")
-
-    let timeAzul = document.createElement("option");
-    timeAzul.value = "1";
-    timeAzul.innerHTML = `&#128309; Time Azul`;
-    timeAzul.style.backgroundColor="steelblue";
-
-    let timeRoxo = document.createElement("option");
-    timeRoxo.value = "2";
-    timeRoxo.innerHTML = `&#128995; Time Roxo`;
-    timeRoxo.style.backgroundColor="slateblue";
-
-    let timeVermelho = document.createElement("option");
-    timeVermelho.value = "3";
-    timeVermelho.innerHTML = `&#128308; Time Vermelho`;
-    timeVermelho.style.backgroundColor="red";
-
-    
-
-    corTime1.add(timeAzul, null);
-    corTime1.add(timeRoxo, null);
-    corTime1.add(timeVermelho, null);
-
-    document.getElementById("allEvents").appendChild(labelCor);
-    document.getElementById("allEvents").appendChild(corTime1);
-
-    var confirmTeamColor = document.createElement("button");
-    confirmTeamColor.innerHTML = "OK";
-    confirmTeamColor.setAttribute("class", "confirm-setup");
-    confirmTeamColor.setAttribute("id", "ConfirmTeamColorID");
-    document.getElementById("allEvents").appendChild(confirmTeamColor);
-    
+    $(function (e) {
+        $("section#infoTable"+matchNumber+"").append("<article id='allEvents' class='all-events'></article>");
+    });
+};
 
 
-}
+/*
+    <label for="exampleInputCountry">Defina o goleiro:</label>
+    <select class="form-control" id="exampleInputCountry" name="country">
+        <option value="" selected="selected" >Selecione o jogador</option>
+        <option value="Adilson">&#128308; Adilson</option>
+        <option value="Ze">&#128308; Zé</option>
+        <option value="Indefinido">Indefinido</option>
+    </select>*/
 
-confirmTeamColor.addEventListener("click", selecionarJogadores());
 
 
-function selecionarJogadores() {
-    alert("funcionou")
-//Adding Team Players VERIFICAR INTEGRIDADE DO BLOCO
+
+
+
+function yy() {
     let labelEscT1P1 = document.createElement("label");
     labelEscT1P1.setAttribute("for", "escT1P1");
     labelEscT1P1.innerHTML = "Selecione o goleiro: ";
@@ -86,7 +195,7 @@ function selecionarJogadores() {
 
     let jog001 = document.createElement("option");
     jog001.value = "1";
-    jog001.innerHTML = `Adilson`; /* &#128309; Acrescentar círculo colorido dinamicamente de acordo com cor de time escolhida */
+    jog001.innerHTML = `Adilson`; 
     jog001.style.backgroundColor="steelblue";
 
     let jog002 = document.createElement("option");
@@ -96,7 +205,7 @@ function selecionarJogadores() {
 
     let jogIndefinido = document.createElement("option");
     jogIndefinido.value = "3";
-    jogIndefinido.innerHTML = `NDEFINIDO`;
+    jogIndefinido.innerHTML = `INDEFINIDO`;
     jogIndefinido.style.backgroundColor="red";
 
 
@@ -113,8 +222,7 @@ function selecionarJogadores() {
     confirmTeamColor.setAttribute ("class", "confirm-setup");
     document.getElementById("allEvents").appendChild(confirmTeamColor);
 // Fim da escolha de jogadores
-}
-
+};
 
 /*
     //Adding Score
@@ -132,7 +240,7 @@ function xx() {
     /*para.className = "texto-campo";*/
     para.setAttribute("id", "placarJogo;")
     document.getElementById("initConfigButton").appendChild(para);
-}
+};
 
 /*
 function initConfig() {
@@ -149,7 +257,7 @@ function testeTexto() {
     let textoInfo = document.getElementById('textoInfo')
     textoInfo.innerHTML = ''
     textoJogo.innerHTML += `ahusahsuasu `
-}
+};
 
 // ETAPA 1 - DEFINIÇÃO DE CORES DOS TIMES
 // ETAPA 2 - DEFINIÇÃO DE JOGADORES DO TIME
@@ -184,7 +292,7 @@ function testeTexto() {
 <div id="placarCentral" style="float: left; text-align: center; width: 30px; margin-bottom: 10px;"><strong>1 x 0</strong></div>
 <div id="time2Placar" style="float: left; text-align: left; width: 115px">&#128309; <strong>Time Azul</strong></div>
 -->
-<article class="all-events">
+<article class="all-events"> 
     
     <div class="texto-campo"> &#128308; Time Vermelho: <strong>Ályson</strong>, <strong>Tuta</strong>, <strong>Fernando</strong>, <strong>Zé</strong>, <strong>Adilson</strong></div>
     <div class="texto-campo"> &#128309; Time Azul: <strong>Dau</strong>, <strong>Milson</strong>, <strong>Ari</strong>, <strong>Iago</strong>, <strong>Antonio</strong></div>
